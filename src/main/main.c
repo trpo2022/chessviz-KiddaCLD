ada0b200dbc17752adb244414ad59cb7c75e8d5f
Губин Артём <hezzebeats@gmail.com>
2022-05-30 04:03:26 +0700

    add start function

--- src/main/main.c
+++ src/main/main.c
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-    char board[9][9]
+    char board[10][10]
             = {"8rnbqkbnr",
                "7pppppppp",
                "6........",
@@ -19,4 +19,12 @@ int main()
     int MoveNumber = 1;
     int j = 1;
     conclusion(board);
+    while (j == 1) {
+        printf("%d.", MoveNumber);
+        WhiteTurn(board);
+        conclusion(board);
+        BlackTurn(board);
+        conclusion(board);
+        MoveNumber += 1;
+    }
 }
