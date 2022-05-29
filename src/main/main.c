09b6c90e7cb0fe0199ff8c8015470a30a3cdb532
Губин Артём <hezzebeats@gmail.com>
2022-05-30 03:09:28 +0700

    add conclusion func

--- src/main/main.c
+++ src/main/main.c
@@ -1,6 +1,8 @@
 #include "../lib/lib.h"
 
-#include <stdio.h>
+#include <iostream>
+
+using namespace std;
 
 int main()
 {
@@ -17,12 +19,4 @@ int main()
     int MoveNumber = 1;
     int j = 1;
     conclusion(board);
-    while (j == 1) {
-        printf("%d.", MoveNumber);
-        WhiteTurn(board);
-        conclusion(board);
-        BlackTurn(board);
-        conclusion(board);
-        MoveNumber += 1;
-    }
 }
