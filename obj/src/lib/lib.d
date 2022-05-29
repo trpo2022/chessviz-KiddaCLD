3f81dca2a8c2b7edae9513a723f9a7044a8521be
Губин Артём <hezzebeats@gmail.com>
2022-05-30 04:31:39 +0700

    fix test

---
+++ obj/src/lib/lib.d
@@ -0,0 +1,3 @@
+obj/src/lib/lib.o: src/lib/lib.c src/lib/lib.h
+
+src/lib/lib.h:
---
+++ obj/test/test.d
@@ -0,0 +1,5 @@
+obj/test/test.o: test/test.c thirdparty/ctest.h src/lib/lib.h
+
+thirdparty/ctest.h:
+
+src/lib/lib.h:
